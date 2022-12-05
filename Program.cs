e24bc238f7801a35543ca556655487b8fa12825c
Avaz <qoraqosh1993@mail.ru>
2022-12-06 00:35:29 +0500

    all done

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int Test(){
+//     int num = new Random().Next(100, 1000);
+//     System.Console.WriteLine(num);
+//     int n1 = num / 100;
+//     int n2 = num / 10 % 10;
+//     int n3 = num % 10;
+//     return n2;
+// }
+// int result = Test();
+// Console.WriteLine(result);
+
+
+
+
+// Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// void Test(){
+//     int num = new Random().Next();
+//     Console.WriteLine(num);
+//     if (num>1000){
+//         {while(num>1000){
+//             num = num / 10;}}
+//         int n2 = num%10;
+//         Console.WriteLine(n2);}
+//     else if (num>100 & num<1000){
+//         int n2 = num%10;
+//         System.Console.WriteLine(n2);
+//     }
+//     else {
+//         System.Console.WriteLine("Третьей цифры не существует!");
+//     }
+// }
+
+// Test();
+
+
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// void Test(){
+//     int num = new Random().Next(1,8);
+//     Console.WriteLine(num);
+//     if (num == 6){
+//         Console.WriteLine("Этот день выходной");
+//     }
+//     else if (num == 7){
+//         Console.WriteLine("Этот день выходной");
+//     }
+//     else
+//         {
+//         Console.WriteLine("Этот день НЕ выходной");
+//     }
+// }
+
+// Test();
